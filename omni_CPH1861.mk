85720409e17eddeda7cf286610a0471c705802c5
devil-black-786 <60068093+devil-black-786@users.noreply.github.com>
2020-07-07 05:58:06 -1000

    Update omni_CPH1861.mk

--- omni_CPH1861.mk
+++ omni_CPH1861.mk
@@ -20,7 +20,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier.
 PRODUCT_BRAND := OPPO
